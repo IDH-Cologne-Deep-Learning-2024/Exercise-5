a0ba1938ea8bc1ee57ad6f5e862b93e115c14660
Lars Bieber <lbieber1@smail.uni-koeln.de>
2024-11-07 15:28:53 +0100

    fix

--- train_keras.py
+++ train_keras.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Input
-import matplotlib.pyplot as plt
 
 titanic = pd.read_csv("titanic.csv")
 
