2452154f56921888faa6131338b29f3da1fd3f75
NP <ninasopopp@gmail.com>
2024-11-11 21:41:02 +0100

    cleaned up incl comments

--- train_keras.py
+++ train_keras.py
@@ -4,20 +4,12 @@ from tensorflow.keras.layers import Dense, Input
 import numpy as np
 import matplotlib.pyplot as plt
 
-# use fit method in keras to train 
-# s. Sequential model: single in-output stacks of layers; 
-# look at weights of each layer before and after training and how they change
-# plus look at loss and if model converges (if so, after how many epochs?)
-
-#keras methods: compile, fit, evaluate, predict, train/test/predict_on_batch
-#keras fit returns History obj 
-#keras fit args: y, y, batch_size, epochs, verbosse, callbakcs, validation_split/data, shuffle, class/sample_weight, initial epoch, steps_per_epoch, validation_steps, validation_batch_size, _validation_freq
-
 def read_data(path):
     dataframe = pd.read_csv(path, sep=",")
     return dataframe
 
-def build(): #batch_input_shape?
+#building model
+def build():
     model = Sequential([
         Input(shape= (2,)),
         Dense(4, activation= 'relu'), 
@@ -27,6 +19,7 @@ def build(): #batch_input_shape?
     model.compile(optimizer='sgd', loss= 'binary_crossentropy')
     return model
 
+#plot changing loss
 def plotten(history):
     plt.plot(history.history['loss'], label= 'Loss')
     plt.ylabel('Loss')
@@ -35,6 +28,7 @@ def plotten(history):
     plt.show()
 
 def main():
+    #setting up df
     dataframe = read_data('titanic.csv')
     dataframe = dataframe[dataframe['Age'].notna()]
     dataframe['Age'] = dataframe['Age'] / 10 #shouldve standardized instead? or in addition
@@ -48,8 +42,11 @@ def main():
     for layer in model.layers:
         weights= layer.get_weights()
         print(weights)
-        
+
+    #fit to train     
     history= model.fit(x, y, epochs= 113, batch_size=23, verbose=1)
+
+    #post-training
     print("Weights post-T: ")
     for layer in model.layers:
         weights= layer.get_weights()
