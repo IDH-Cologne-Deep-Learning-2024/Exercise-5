3516c3ebaf7b10ed163ec6095addc0041e96ba53
graef <simonjn45@gmail.com>
2024-11-13 16:50:22 +0100

    done

--- train_keras.py
+++ train_keras.py
@@ -1,3 +1,21 @@
 import pandas as pd
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Input
+
+df = pd.read_csv("titanic.csv", header=0)
+
+model = Sequential()
+model.add(Input(shape=(2,)))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(3, activation='relu'))
+model.add(Dense(2, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',optimizer="sgd")
+
+model.summary()
+
+x = df[["Age", "Pclass"]].values
+y = df["Survived"].values
+
+model.fit(x=x,y=y,epochs=100,batch_size=80)
