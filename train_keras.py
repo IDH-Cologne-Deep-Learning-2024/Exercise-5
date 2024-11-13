f7b142bc1ec90bef7c09854c5476200b0c0c5798
ryuunosukeke <melissa.eleanya@yahoo.com>
2024-11-13 15:21:51 +0000

    fertig

--- train_keras.py
+++ train_keras.py
@@ -1,3 +1,40 @@
 import pandas as pd
+import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Input
+
+titanic = pd.read_csv("titanic.csv")
+
+titanic = titanic[['Age', 'Pclass', 'Survived']].dropna()
+X = titanic[['Age', 'Pclass']].values #featrures
+Y = titanic['Survived'].values #target
+
+model = Sequential([
+    Input(shape =(2,)),
+    Dense(5, activation='sigmoid'),
+    Dense(3, activation="sigmoid"),
+    Dense(1, activation="relu")])
+
+model.compile(loss='binary_crossentropy', optimizer="sgd")
+
+print("Initial weights of each layer:")
+for layer in model.layers:
+    weights, biases = layer.get_weights()
+    print(f"Weights: {weights}, Biases: {biases}")
+
+history = model.fit(X, Y, epochs=200, batch_size=64, validation_split=0.2, verbose=1)
+
+print("\nWeights after training:")
+for layer in model.layers:
+    weights, biases = layer.get_weights()
+    print(f"Weights: {weights}, Biases: {biases}")
+  
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+print("\nFinal Training Loss:", loss[-1])
+print("Final Validation Loss:", val_loss[-1])
+
+converged_epochs = np.argmin(val_loss) + 1 
+print(f"The model converged after {converged_epochs} epochs with the minimum validation loss of {min(val_loss)}.")
+
