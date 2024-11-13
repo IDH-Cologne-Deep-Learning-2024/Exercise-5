7803cba9d9f98a5e4fedba45949955f6ad704481
graef <simonjn45@gmail.com>
2024-11-13 16:58:29 +0100

    added printing weights

--- train_keras.py
+++ train_keras.py
@@ -7,15 +7,22 @@ df = pd.read_csv("titanic.csv", header=0)
 model = Sequential()
 model.add(Input(shape=(2,)))
 model.add(Dense(4, activation='relu'))
-model.add(Dense(3, activation='relu'))
-model.add(Dense(2, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',optimizer="sgd")
 
-model.summary()
+# model.summary()
 
 x = df[["Age", "Pclass"]].values
 y = df["Survived"].values
 
-model.fit(x=x,y=y,epochs=100,batch_size=80)
+for layer in model.layers:
+    w, _ = layer.get_weights()
+    print(f"{w}")
+
+model.fit(x=x,y=y,epochs=4,batch_size=200)
+
+
+for layer in model.layers:
+    w, _ = layer.get_weights()
+    print(f"{w}")
